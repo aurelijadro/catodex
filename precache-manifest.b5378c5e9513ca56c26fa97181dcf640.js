dc2810246f169b848308681aaee0b3d2a6a9cab8
Aurelija <a.drobelyte@gmail.com>
2019-11-29 11:05:43 +0200

    Updates

---
+++ precache-manifest.b5378c5e9513ca56c26fa97181dcf640.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e8748a282687264269462472d17abba",
+    "url": "/catodex/index.html"
+  },
+  {
+    "revision": "b97809ffeadbe8301be5",
+    "url": "/catodex/static/css/main.ad0270f1.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/catodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "b97809ffeadbe8301be5",
+    "url": "/catodex/static/js/main.aa4abf41.chunk.js"
+  },
+  {
+    "revision": "180c59e8718e7db9be99",
+    "url": "/catodex/static/js/runtime-main.afa5dd66.js"
+  }
+]);
